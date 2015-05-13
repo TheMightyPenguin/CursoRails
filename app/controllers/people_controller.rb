6be16f869af5ed6079c786054aed31d0e4dbd9dc
maitzeth <andre_801@hotmail.com>
2015-05-12 20:56:31 -0430

    Agrego nuevos controladores, agregar, editar

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,7 +4,45 @@ class PeopleController < ApplicationController
     @people = Person.all
   end
 
+  # Select
   def show
     @person = Person.find(params[:id])
+  end
+
+  #Insert
+  def new
+    @person = Person.new
+    @m = "crear persona"
+  end
+
+  def create
+    @person = Person.new(permit_params)
+
+    if @person.save
+      redirect_to people_path, notice: "Persona creada correctamente"
+    else
+      render 'new'
+    end
+  end
+
+  #Update
+  def edit
+    @person = Person.find(params[:id])
+    @m = "editar-persona"
+  end
+
+  def update
+    @person = Person.find(params[:id])
+
+    if @person.update(permit_params)
+      redirect_to people_path, notice: "#{@person.nombre} Editada Exitosamente"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def permit_params
+    params.require(:person).permit(:nombre, :apellido, :ci)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :people do
     resources :addresses
   end
+
   
+
   #get "/people" => "people#index"
   root to: "people#index"
 end
