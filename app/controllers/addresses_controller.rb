4ab648373efff78c308a5b47d0be37b6c6237a32
maitzeth <andre_801@hotmail.com>
2015-05-16 03:07:55 -0430

    Agrega addresses_controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,7 +4,10 @@ class AddressesController < ApplicationController
     address = Address.find(params[:id])
     if address.destroy
       flash[:notice] = "Fue eliminado exitosamente"
-      redirect_to person_path(params[:person_id])  
+      redirect_to person_path(params[:person_id])
     end
+  end
+  def new_person_address
+      Person.addresses.build(params[:id])
   end
 end
